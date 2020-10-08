24b0a229220e1468f38afd5ea5b2b3f47e536911
Thomas Axelsson <thomasa88@gmail.com>
2020-10-08 08:26:24 +0200

    Implementation

--- NoComponentDrag.py
+++ NoComponentDrag.py
@@ -1,8 +1,8 @@
 #Author-Thomas Axelsson
 #Description-Blocks Component Dragging in parametric mode
 
-# This file is part of AnyShortcut, a Fusion 360 add-in for assigning
-# shortcuts to the last run commands.
+# This file is part of NoComponentDrag, a Fusion 360 add-in for blocking
+# component drags.
 #
 # Copyright (c) 2020 Thomas Axelsson
 # 
@@ -50,6 +50,8 @@ importlib.reload(thomasa88lib.manifest)
 importlib.reload(thomasa88lib.error)
 importlib.reload(thomasa88lib.timeline)
 
+ENABLE_CMD_ID = 'thomasa88_NoComponentDrag_Enable'
+SEPARATOR_ID = 'thomasa88_NoComponentDrag_SeparatorAfter'
 
 app_ = None
 ui_ = None
@@ -57,28 +59,93 @@ error_catcher_ = thomasa88lib.error.ErrorCatcher()
 events_manager_ = thomasa88lib.events.EventsManager(error_catcher_)
 manifest_ = thomasa88lib.manifest.read()
 
+enable_cmd_def_ = None
+drag_enabled_ = True
+right_environment_ = True
+
 def command_starting_handler(args: adsk.core.ApplicationCommandEventArgs):
-    print("START: ", args.commandId)
-    if args.commandId == 'FusionDragComponentsCommand':
+    if right_environment_ and not drag_enabled_ and args.commandId == 'FusionDragComponentsCommand':
         args.isCanceled = True
 
-def command_created_handler(args: adsk.core.ApplicationCommandEventArgs):
-    print("CREATE:", args.commandId)
+def enable_cmd_created_handler(args: adsk.core.CommandCreatedEventArgs):
+    global drag_enabled_
+    checkbox_def: adsk.core.CheckBoxControlDefinition = args.command.parentCommandDefinition.controlDefinition
+    if checkbox_def.isChecked:
+        drag_enabled_ = True
+    else:
+        drag_enabled_ = False
 
-def command_terminated_handler(args: adsk.core.ApplicationCommandEventArgs):
-    print("TERM:  ", args.commandId, args.terminationReason)
+def document_activated_handler(args: adsk.core.WorkspaceEventArgs):
+    toggle_visibility()
+
+def toggle_visibility():
+    # Don't make a double command in the direct editing environment
+    global enable_cmd_def_
+    global right_environment_
+    
+    show = False
+    if ui_.activeWorkspace.id == 'FusionSolidEnvironment':
+        design: adsk.fusion.Design = app_.activeProduct
+        if design.designType == adsk.fusion.DesignTypes.ParametricDesignType:
+            show = True
+    enable_cmd_def_.controlDefinition.isVisible = show
+    right_environment_ = show
 
 def run(context):
     global app_
     global ui_
+    global enable_cmd_def_
     with error_catcher_:
         app_ = adsk.core.Application.get()
         ui_ = app_.userInterface
 
+        enable_cmd_def_ = ui_.commandDefinitions.itemById(ENABLE_CMD_ID)
+        if enable_cmd_def_:
+            enable_cmd_def_.deleteMe()
+
+        # Use a Command to get a transaction when renaming
+        enable_cmd_def_ = ui_.commandDefinitions.addCheckBoxDefinition(ENABLE_CMD_ID,
+                                                                 f'Component Drag',
+                                                                 'Enables or disables the movement of components by dragging'
+                                                                  'in the canvas.\n\n'
+                                                                  f'({NAME} v {manifest_["version"]})',
+                                                                  True)
+        events_manager_.add_handler(enable_cmd_def_.commandCreated,
+                                    callback=enable_cmd_created_handler)
+
+        panel_id = 'SelectPanel'
+        # There are multiple select panels. Pick the right one
+        panel = ui_.toolbarPanelsByProductType('DesignProductType').itemById(panel_id)
+        old_control = panel.controls.itemById(ENABLE_CMD_ID)
+        if old_control:
+            old_control.deleteMe()
+        panel.controls.addCommand(enable_cmd_def_, 'SeparatorAfter_SelectionToolsDropDown', False)
+
+        old_control = panel.controls.itemById(SEPARATOR_ID)
+        if old_control:
+            old_control.deleteMe()
+        panel.controls.addSeparator(SEPARATOR_ID, ENABLE_CMD_ID, False)
+
         events_manager_.add_handler(ui_.commandStarting, callback=command_starting_handler)
-        events_manager_.add_handler(ui_.commandCreated, callback=command_created_handler)
-        events_manager_.add_handler(ui_.commandTerminated, callback=command_terminated_handler)
+
+        # Fusion bug: DocumentActivated is not called when switching to/from Drawing.
+        # https://forums.autodesk.com/t5/fusion-360-api-and-scripts/api-bug-application-documentactivated-event-do-not-raise/m-p/9020750
+        events_manager_.add_handler(app_.documentActivated,
+                                    callback=document_activated_handler)
+
+        # Workspace is not ready when starting (?)
+        if app_.isStartupComplete:
+            toggle_visibility()
 
 def stop(context):
     with error_catcher_:
         events_manager_.clean_up()
+
+        panel = ui_.toolbarPanelsByProductType('DesignProductType').itemById(panel_id)
+        old_control = panel.controls.itemById(ENABLE_CMD_ID)
+        if old_control:
+            old_control.deleteMe()
+
+        old_control = panel.controls.itemById(SEPARATOR_ID)
+        if old_control:
+            old_control.deleteMe()
---
+++ README.md
@@ -0,0 +1,47 @@
+# NoComponentDrag
+
+A Fusion 360 add-in that adds a *Component Drag* option in the parametric design environment. It works just like the *Component Drag* option in the direct editing environment.
+
+
+
+## Supported Platforms
+
+* Windows
+* Mac OS
+
+## Installation
+
+Download the add-in from the [Releases](https://github.com/thomasa88/NoComponentDrag/releases) page.
+
+Unpack it into `API\AddIns` (see [How to install an add-in or script in Fusion 360](https://knowledge.autodesk.com/support/fusion-360/troubleshooting/caas/sfdcarticles/sfdcarticles/How-to-install-an-ADD-IN-and-Script-in-Fusion-360.html)).
+
+Make sure the directory is named `NoComponentDrag`, with no suffix.
+
+## Usage
+
+Select/unselect *Component Drag* in the Select dropdown.
+
+## Reporting Issues
+
+Please report any issues that you find on the [Issues](https://github.com/thomasa88/NoComponentDrag/issues) page.
+
+For better support, please include the steps you performed and the result. Also include copies of any error messages.
+
+## Author
+
+This add-in is created by Thomas Axelsson.
+
+## License
+
+This project is licensed under the terms of the MIT license. See [LICENSE](LICENSE).
+
+## More Fusion 360™ Add-ins
+
+[My Fusion 360™ app store page](https://apps.autodesk.com/en/Publisher/PublisherHomepage?ID=JLH9M8296BET)
+
+[All my add-ins on Github](https://github.com/topics/fusion-360?q=user%3Athomasa88)
+
+## Changelog
+
+* v 0.10
+  * First beta
